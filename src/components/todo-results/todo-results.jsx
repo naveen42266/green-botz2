c49b265e78e2e1fd59f7b6ccbda951180567caae
Naveen <vnaveenlgp2001@gmail.com>
2024-05-17 16:56:10 +0530

    third commit

--- src/components/todo-results/todo-results.jsx
+++ src/components/todo-results/todo-results.jsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import './todo-results.scss';
+import { TodosContext } from '../../todo-context';
 
 export const TodoResults = () => {
+  const { todos, setTodos } = React.useContext(TodosContext);
+
   const calculateChecked = () => {
     // Fix an ability to calculate completed tasks
+    let checkedCount = 0;
+    todos.forEach((todo) => {
+      if (todo.checked) {
+        checkedCount += 1;
+      }
+    });
+
+    return checkedCount;
   };
 
   return (
